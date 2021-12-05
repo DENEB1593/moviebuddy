dc97c76434b81143ba5ef5dadfd89bf66337c0b9
lee kangwoon <deneb1593@gmail.com>
2021-12-05 21:48:33 +0900

    > 스프링 자바 빈 등록

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,17 +2,35 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 // 빈 메타정보를 설정하며..
 @Configuration
+// Import를 사용하면 다른 설정(Configuaration)에서 Bean정보를 불러옴
+@Import({MovieBuddyFactory.DomainModuleConfig.class
+        , MovieBuddyFactory.DataSourceModule.class})
 public class MovieBuddyFactory {
 
-    // 스프링 컨테이너에 빈을 등록한다.
-    @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    // 내부 설정(Configuration) 등록
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")  // Intellij 버그용 어노테이션
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
     }
 
+    @Configuration
+    static class DataSourceModule {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
+    }
 }
